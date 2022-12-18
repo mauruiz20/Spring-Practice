9630cc921298713da30c16a82647ee750f482b91
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-12-17 21:30:55 -0300

    SeguridadSpring

---
+++ SeguridadSpring/src/main/java/es/pildoras/SeguridadSpring/App.java
@@ -0,0 +1,13 @@
+package es.pildoras.SeguridadSpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
